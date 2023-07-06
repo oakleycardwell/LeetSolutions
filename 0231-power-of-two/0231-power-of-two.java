47b6f2b1cfa0cf33b91abe0dcdaff10eb61447bb
Oakley Cardwell <46533156+oakleycardwell@users.noreply.github.com>
2023-07-06 09:42:35 -0500

    Time: 1 ms (100%), Space: 39.3 MB (96.45%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        long num = 1;
+
+        for (int i = 0; i < 32; i++){
+            if (num == n){
+                return true;
+            }
+            num *= 2;
+        }
+
+        return false;
+
+
+    }
+}
