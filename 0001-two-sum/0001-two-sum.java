58cb631032dd57716e55fcd0e6c276852bc56fd2
Oakley Cardwell <46533156+oakleycardwell@users.noreply.github.com>
2023-07-19 17:38:50 -0500

    Time: 2 ms (81.06%), Space: 43.9 MB (51.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int len = nums.length;
-        
-        for (int i = 0; i < len; i++){
-            for (int j = 0; j < len; j++){
-                if (nums[i] + nums[j] == target){
-                    if(i != j)
-                        return new int[] {i , j};
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < len; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+            map.put(nums[i], i);
         }
-        
-        return new int[] {-1, -1};
+
+        return new int[] { -1, -1 };
     }
 }
