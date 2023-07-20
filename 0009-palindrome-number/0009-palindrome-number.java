8ac9804a09a36d9b7a1b3eb968605a28fe0572a5
Oakley Cardwell <46533156+oakleycardwell@users.noreply.github.com>
2023-07-20 10:18:41 -0500

    Time: 9 ms (99.62%), Space: 43 MB (50.76%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String forwards = "" + x;
-        int len = forwards.length();
-        
-        int j = len-1;
-        for (int i = 0; i < len; i++){
-            if (forwards.charAt(i) != forwards.charAt(j))
-                return false;
-            j--;
+        if (x < 0)
+            return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
-        
-        return true;
+
+        return original == reversed;
     }
 }
