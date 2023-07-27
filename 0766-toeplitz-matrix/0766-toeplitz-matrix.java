a477330845719034ad785428ec166d3326aefe76
Oakley Cardwell <46533156+oakleycardwell@users.noreply.github.com>
2023-07-27 14:59:11 -0500

    Time: 1 ms (75.06%), Space: 42.9 MB (93.34%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static boolean isToeplitzMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (int row = 0; row < rows - 1; row++) {
+            for (int col = 0; col < cols - 1; col++) {
+                if (matrix[row][col] != matrix[row + 1][col + 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
