c9d648e5109dcdea0b40d8e5ec98f386d92fa126
Oakley Cardwell <46533156+oakleycardwell@users.noreply.github.com>
2023-08-04 19:24:57 -0500

    Time: 2 ms (90.99%), Space: 40.8 MB (30.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean isValid(String s) {
+        int length = s.length();
+        if (length == 0)
+            return true;
+        else if (length == 1)
+            return false;
+        
+        
+        Stack<Character> stack = new Stack();
+        
+        stack.push(s.charAt(0));
+        
+        for (int i = 1; i < s.length(); i++){
+            char nextChar = s.charAt(i);
+            
+            if (nextChar == '{' || nextChar == '[' || nextChar == '('){
+                stack.push(nextChar);
+            }
+            else{
+                if (stack.size() > 0){
+                    char peek = stack.peek();
+                    switch(nextChar){
+                        case '}':
+                            if (peek == '{')
+                                stack.pop();
+                            else
+                                return false;
+                            break;
+                        case ']':
+                            if (peek == '[')
+                                stack.pop();
+                            else
+                                return false;
+                            break;
+                        case ')':
+                            if (peek == '(')
+                                stack.pop();
+                            else
+                                return false;
+                            break;
+                        default:
+                            return false;
+                    }
+                }
+                else
+                    return false;
+            }
+        }
+        
+        if (stack.size() > 0)
+            return false;
+        
+        return true;
+    }
+}
