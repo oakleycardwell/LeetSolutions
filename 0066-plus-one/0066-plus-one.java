24bd07d1b071dc47e2c53926984f4da9543c4170
Oakley Cardwell <46533156+oakleycardwell@users.noreply.github.com>
2024-02-06 12:59:31 -0600

    Time: 0 ms (100.00%), Space: 42.1 MB (6.32%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,23 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int i = digits.length - 1;
-        digits[i]++;
-        while (digits[i] >= 10){
-            digits[i] = 0;
-            i--;
-            if (i < 0){
-                int[] newDigits = new int[digits.length+1];
-                newDigits[0] = 1;
-                for (int j = 1; j < newDigits.length; j++){
-                    System.out.println("J: "+ j);
-                    newDigits[j] = digits[j-1];
-                }
-                return newDigits;
-            }
-            else{
+        for (int i = digits.length - 1; i >= 0; i--) {
+            // Increment the last digit and check if there's a carry
+            if (digits[i] < 9) {
                 digits[i]++;
+                return digits;
             }
+            // If the digit is 9, it becomes 0 (carry over to the next significant digit)
+            digits[i] = 0;
         }
-        return digits;
+        
+        // If all digits were 9, the code reaches here. Need to add a new digit at the beginning
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        // No need to set other digits to 0 as int arrays are initialized with 0 in Java
+        return newDigits;
     }
 }
