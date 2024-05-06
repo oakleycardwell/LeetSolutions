47c80eef659cb9a86b7d605462278fd6b44b14b4
Oakley Cardwell <46533156+oakleycardwell@users.noreply.github.com>
2024-05-06 16:10:35 -0500

    Time: 1 ms (98.75%), Space: 41.6 MB (92.69%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPathCrossing(String path) {
         // O(1) average for insert and look up
-        Set<String> visited = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
         // Start at the origin
         int x = 0, y = 0;
         // Add the starting position to the set
-        visited.add(x + "," + y);
+        visited.add(0);
         
         for (char direction : path.toCharArray()) {
             // Update coordinates based on the direction
@@ -23,15 +23,16 @@ class Solution {
                     x--;
                     break;
             }
-            // Check if the new position is already visited
-            String pos = x + "," + y;
-            if (visited.contains(pos)) {
-                return true;
+            
+            // Hash value, large enough that coordinates do not intersect
+            int hash = y * 10000 + x;
+            
+            // Check if the new position is already visited, if not add to set
+            if (!visited.add(hash)) {
+                return true; // Return if the path crosses itself
             }
-            // Add current position to set
-            visited.add(pos);
         }
-        
+        // No crossed paths found
         return false;
     }
 }
